13974c4e7f61503027ac6b6a9c3029675678fbe3
FLI\nbaynham <nbaynham@flhi.com>
2019-10-29 16:00:51 -0400

    added RegEx package

---
+++ src/main/java/io/nickbaynham/automation/regex/RegEx.java
@@ -0,0 +1,5 @@
+package io.nickbaynham.automation.regex;
+
+public class RegEx {
+
+}
---
+++ src/test/java/io/nickbaynham/automation/regex/TestRegEx.java
@@ -0,0 +1,10 @@
+package io.nickbaynham.automation.regex;
+
+import org.testng.annotations.Test;
+
+public class TestRegEx {
+    @Test
+    public void testRegEx() {
+
+    }
+}

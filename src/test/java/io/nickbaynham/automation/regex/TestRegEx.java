fb3396a6a298ae02e4060a24032c1c1fcde212af
FLI\nbaynham <nbaynham@flhi.com>
2019-10-29 17:35:08 -0400

    added new RegEx Examples

--- src/test/java/io/nickbaynham/automation/regex/TestRegEx.java
+++ src/test/java/io/nickbaynham/automation/regex/TestRegEx.java
@@ -53,5 +53,70 @@ public class TestRegEx {
         int matches = getNumberOfMatches("bat cat rat","[bcr]at");
         assertEquals(matches, 3);
     }
+
+    @Test
+    public void testNORSet() {
+        int matches = getNumberOfMatches("g", "[^abc]");
+        assertEquals(matches, 1);
+    }
+
+    @Test
+    public void testNORSet2() {
+        int matches = getNumberOfMatches("sat mat eat", "[^bcr]at");
+        assertEquals(matches, 3);
+    }
+
+    @Test
+    public void testUppercaseWithRanges() {
+        int matches = getNumberOfMatches("Two Uppercase alphabets 34 overall", "[a-z]");
+        assertEquals(matches, 26);
+    }
+
+    @Test
+    public void testUppercaseLowercaseWithRanges() {
+        int matches = getNumberOfMatches("Two Uppercase alphabets 34 overall", "[aA-zZ]");
+        assertEquals(matches, 28);
+    }
+
+    @Test
+    public void testNumbersWithRanges() {
+        int matches = getNumberOfMatches("Two Uppercase alphabets 34 overall", "[1-5]");
+        assertEquals(matches, 2);
+    }
+
+    @Test
+    public void testNumbersWithAnotherRange() {
+        int matches = getNumberOfMatches("Two Uppercase alphabets 34 overall", "[30-35]");
+        assertEquals(matches, 1);
+    }
+
+    @Test
+    public void testPredefinedCharacterClasses() {
+        assertEquals(getNumberOfMatches("123", "\\d"), 3);      // Digit
+        assertEquals(getNumberOfMatches("a6c", "\\D"), 2);      // Non Digit
+        assertEquals(getNumberOfMatches("a c", "\\s"), 1);      // White Space Characters
+        assertEquals(getNumberOfMatches("a c", "\\S"), 2);      // Non White Space
+        assertEquals(getNumberOfMatches("hi!", "\\W"), 1);      // Non Word Character
+    }
+
+    @Test
+    public void testZeroOrOneQuantifier() {
+        assertEquals(getNumberOfMatches("hi", "\\a?"), 3);
+        assertEquals(getNumberOfMatches("hi", "\\a{0,1}"), 3);
+    }
+
+    @Test
+    public void testOneOrManyQuantifier() {
+        assertFalse(getNumberOfMatches("hi", "\\a+")>0);
+        assertEquals(getNumberOfMatches("hi", "\\a{1,}"),0);
+    }
+
+    @Test
+    public void testBraceQuantifier() {
+        assertEquals(getNumberOfMatches("aaaaaa", "a{3}"), 2);
+        assertEquals(getNumberOfMatches("aa", "a{3}"), 0);
+        assertEquals(getNumberOfMatches("aaaa", "a{2,3}"), 1);
+        assertEquals(getNumberOfMatches("aaaa", "a{2,3}?"), 2);
+    }
 }
 

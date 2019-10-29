7a8dc94496c51f71e3606cbd4722ceed6a18af16
FLI\nbaynham <nbaynham@flhi.com>
2019-10-29 16:24:44 -0400

    added RegEx examples with dot meta character

--- src/main/java/io/nickbaynham/automation/regex/RegEx.java
+++ src/main/java/io/nickbaynham/automation/regex/RegEx.java
@@ -1,5 +1,23 @@
 package io.nickbaynham.automation.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegEx {
 
+    public static boolean isMatch(String target, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(target);
+        return matcher.find();
+    }
+
+    public static int getNumberOfMatches(String target, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(target);
+        int matches = 0;
+        while (matcher.find()) {
+            matches++;
+        }
+        return matches;
+    }
 }
--- src/test/java/io/nickbaynham/automation/regex/TestRegEx.java
+++ src/test/java/io/nickbaynham/automation/regex/TestRegEx.java
@@ -1,10 +1,26 @@
 package io.nickbaynham.automation.regex;
 
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 public class TestRegEx {
     @Test
-    public void testRegEx() {
+    public void testRegExIsMatch() {
+        assertTrue(RegEx.isMatch("foo", "foo"));
+    }
 
+    @Test
+    public void testRegExGetNumberOfMatches() {
+        assertEquals(RegEx.getNumberOfMatches("foofoo", "foo"), 2);
+    }
+
+    @Test
+    public void testRegExDotMatching() {
+        assertTrue(RegEx.getNumberOfMatches("foo", ".") > 0);
+    }
+
+    @Test
+    public void testRegExDotMatchingOnce() {
+        assertEquals(RegEx.getNumberOfMatches("foofoo", "foo."), 1);
     }
 }
